2fe9ab32a5e6ac5d2d56dbcff3ce8cd246eda641
omarfarukesham <omar.lu86@gmail.com>
2025-08-16 11:19:05 +0600

    feat: one hydration issue has fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={figtree.variable}>
       <body className={`${figtree.variable} antialiased`}>
         {children}
       </body>
